c31a604f0cf03b62655b360a0ffe6ad63397604a
Ben Weeks <ben.weeks@outlook.com>
2023-12-31 23:54:22 +0000

    Refactored saveApiKey to fetchBalance for refreshing balance.

--- client/src/pages/admin/LightningWallet.js
+++ client/src/pages/admin/LightningWallet.js
@@ -4,7 +4,18 @@ import { TextField, PrimaryButton } from '@fluentui/react';
 const LightningWallet = () => {
   const [balance, setBalance] = useState(0);
 
-  const saveApiKey = async () => {
+  const fetchBalance = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/zebedee/balance`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setBalance(data.data.balance);
+    } catch (error) {
+      console.log('There was a problem with the fetch operation: ' + error.message);
+    }
+  };
     // Save the API key
     console.log(apiKey);
 
@@ -35,7 +46,7 @@ const LightningWallet = () => {
     <div>
       <h1>Lightning Wallet</h1>
       <p>Balance: {balance}</p>
-      <PrimaryButton onClick={saveApiKey}>Refresh Balance</PrimaryButton>
+      <PrimaryButton onClick={fetchBalance}>Refresh Balance</PrimaryButton>
     </div>
   );
 };
