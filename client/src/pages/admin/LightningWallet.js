5cc17d75e17e12c0431f385351278b0ab1908103
Ben Weeks <ben.weeks@outlook.com>
2023-12-31 22:50:12 +0000

    aider: Refactored LightningWallet component to remove unnecessary wrapping and import.
    # Aider chat conversation:
    
    USER: OK, the page opens and I see the menu, but nothing about the wallet.
    
    ASSISTANT: It seems like there is a problem with the `LightningWallet.js` file. The code for the LightningWallet component is wrapped inside another component. This is likely causing the issue.
    
    Let's correct this by modifying the `client/src/pages/admin/LightningWallet.js` file:
    
    1. Remove the outer LightningWallet component.
    2. Remove the unnecessary import statement inside the component.
    
    Here is the *SEARCH/REPLACE* block:
    
    client/src/pages/admin/LightningWallet.js
    ```javascript
    <<<<<<< SEARCH
    import React from 'react';
    
    const LightningWallet = () => {
      return (
        <div>
          <h1>Lightning Wallet</h1>
          import { useState } from 'react';
          import { TextField, PrimaryButton } from '@fluentui/react';
    
          const LightningWallet = () => {
            const [walletName, setWalletName] = useState('');
            const [walletPassword, setWalletPassword] = useState('');
    
            const createWallet = async () => {
              const response = await fetch('https://api.zebedee.io/v0/wallet', {
                method: 'POST',
                headers: {
                  'Content-Type': 'application/json',
                  'apikey': process.env.REACT_APP_ZEBEDEE_API_KEY,
                },
                body: JSON.stringify({
                  name: walletName,
                  password: walletPassword,
                }),
              });
    
              const data = await response.json();
              console.log(data);
            };
    
            return (
              <div>
                <h1>Lightning Wallet</h1>
                <TextField label="Wallet Name" value={walletName} onChange={(e, newValue) => setWalletName(newValue)} />
                <TextField label="Wallet Password" value={walletPassword} onChange={(e, newValue) => setWalletPassword(newValue)} />
                <PrimaryButton onClick={createWallet}>Create Wallet</PrimaryButton>
              </div>
            );
          };
        </div>
      );
    };
    
    export default LightningWallet;
    =======
    import React, { useState } from 'react';
    import { TextField, PrimaryButton } from '@fluentui/react';
    
    const LightningWallet = () => {
      const [walletName, setWalletName] = useState('');
      const [walletPassword, setWalletPassword] = useState('');
    
      const createWallet = async () => {
        const response = await fetch('https://api.zebedee.io/v0/wallet', {
          method: 'POST',
          headers: {
            'Content-Type': 'application/json',
            'apikey': process.env.REACT_APP_ZEBEDEE_API_KEY,
          },
          body: JSON.stringify({
            name: walletName,
            password: walletPassword,
          }),
        });
    
        const data = await response.json();
        console.log(data);
      };
    
      return (
        <div>
          <h1>Lightning Wallet</h1>
          <TextField label="Wallet Name" value={walletName} onChange={(e, newValue) => setWalletName(newValue)} />
          <TextField label="Wallet Password" value={walletPassword} onChange={(e, newValue) => setWalletPassword(newValue)} />
          <PrimaryButton onClick={createWallet}>Create Wallet</PrimaryButton>
        </div>
      );
    };
    
    export default LightningWallet;
    >>>>>>> REPLACE
    ```

--- client/src/pages/admin/LightningWallet.js
+++ client/src/pages/admin/LightningWallet.js
@@ -1,42 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, PrimaryButton } from '@fluentui/react';
 
 const LightningWallet = () => {
+  const [walletName, setWalletName] = useState('');
+  const [walletPassword, setWalletPassword] = useState('');
+
+  const createWallet = async () => {
+    const response = await fetch('https://api.zebedee.io/v0/wallet', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'apikey': process.env.REACT_APP_ZEBEDEE_API_KEY,
+      },
+      body: JSON.stringify({
+        name: walletName,
+        password: walletPassword,
+      }),
+    });
+
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div>
       <h1>Lightning Wallet</h1>
-      import { useState } from 'react';
-      import { TextField, PrimaryButton } from '@fluentui/react';
-
-      const LightningWallet = () => {
-        const [walletName, setWalletName] = useState('');
-        const [walletPassword, setWalletPassword] = useState('');
-
-        const createWallet = async () => {
-          const response = await fetch('https://api.zebedee.io/v0/wallet', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'apikey': process.env.REACT_APP_ZEBEDEE_API_KEY,
-            },
-            body: JSON.stringify({
-              name: walletName,
-              password: walletPassword,
-            }),
-          });
-
-          const data = await response.json();
-          console.log(data);
-        };
-
-        return (
-          <div>
-            <h1>Lightning Wallet</h1>
-            <TextField label="Wallet Name" value={walletName} onChange={(e, newValue) => setWalletName(newValue)} />
-            <TextField label="Wallet Password" value={walletPassword} onChange={(e, newValue) => setWalletPassword(newValue)} />
-            <PrimaryButton onClick={createWallet}>Create Wallet</PrimaryButton>
-          </div>
-        );
-      };
+      <TextField label="Wallet Name" value={walletName} onChange={(e, newValue) => setWalletName(newValue)} />
+      <TextField label="Wallet Password" value={walletPassword} onChange={(e, newValue) => setWalletPassword(newValue)} />
+      <PrimaryButton onClick={createWallet}>Create Wallet</PrimaryButton>
     </div>
   );
 };
