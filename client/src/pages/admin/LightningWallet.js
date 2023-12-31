da2f5d944a142457d40b011300a14ca61086b587
Ben Weeks <ben.weeks@outlook.com>
2023-12-31 23:21:18 +0000

    aider: Display wallet balance from Zebedee API and fetch balance on component mount.
    # Aider chat conversation:
    
    USER: Also display the wallet balance on this screen (use the Zebedee API).
    
    ASSISTANT: To display the wallet balance on the screen, we need to make a request to the Zebedee API. We'll use the `fetch` function to make this request, and the `useEffect` hook to make the request when the component mounts. We'll also need to store the balance in a state variable so we can display it in the component.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    client/src/pages/admin/LightningWallet.js
    ```python
    <<<<<<< SEARCH
    import React, { useState } from 'react';
    =======
    import React, { useState, useEffect } from 'react';
    >>>>>>> REPLACE
    ```
    
    client/src/pages/admin/LightningWallet.js
    ```python
    <<<<<<< SEARCH
    const LightningWallet = () => {
      const [apiKey, setApiKey] = useState('');
    =======
    const LightningWallet = () => {
      const [apiKey, setApiKey] = useState('');
      const [balance, setBalance] = useState(0);
    >>>>>>> REPLACE
    ```
    
    client/src/pages/admin/LightningWallet.js
    ```python
    <<<<<<< SEARCH
      const saveApiKey = () => {
        // Save the API key
        console.log(apiKey);
      };
    =======
      const saveApiKey = async () => {
        // Save the API key
        console.log(apiKey);
    
        // Fetch the balance from the Zebedee API
        const response = await fetch('https://api.zebedee.io/v0/wallet/balance', {
          headers: {
            'Content-Type': 'application/json',
            'api-key': apiKey,
          },
        });
        const data = await response.json();
        setBalance(data.balance);
      };
    
      // Fetch the balance when the component mounts
      useEffect(() => {
        saveApiKey();
      }, []);
    >>>>>>> REPLACE
    ```
    
    client/src/pages/admin/LightningWallet.js
    ```python
    <<<<<<< SEARCH
          <PrimaryButton onClick={saveApiKey}>Save API Key</PrimaryButton>
    =======
          <PrimaryButton onClick={saveApiKey}>Save API Key</PrimaryButton>
          <p>Balance: {balance}</p>
    >>>>>>> REPLACE
    ```
    
    Please note that this code assumes that the Zebedee API key is stored in the `apiKey` state variable, and that the Zebedee API returns the balance in a property named `balance` in the response body. If this is not the case, you may need to adjust the code accordingly.

--- client/src/pages/admin/LightningWallet.js
+++ client/src/pages/admin/LightningWallet.js
@@ -1,19 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, PrimaryButton } from '@fluentui/react';
 
 const LightningWallet = () => {
   const [apiKey, setApiKey] = useState('');
+  const [balance, setBalance] = useState(0);
 
-  const saveApiKey = () => {
+  const saveApiKey = async () => {
     // Save the API key
     console.log(apiKey);
+
+    // Fetch the balance from the Zebedee API
+    const response = await fetch('https://api.zebedee.io/v0/wallet/balance', {
+      headers: {
+        'Content-Type': 'application/json',
+        'api-key': apiKey,
+      },
+    });
+    const data = await response.json();
+    setBalance(data.balance);
   };
+
+  // Fetch the balance when the component mounts
+  useEffect(() => {
+    saveApiKey();
+  }, []);
 
   return (
     <div>
       <h1>Lightning Wallet</h1>
       <TextField label="Zebedee API Key" value={apiKey} onChange={(e, newValue) => setApiKey(newValue)} />
       <PrimaryButton onClick={saveApiKey}>Save API Key</PrimaryButton>
+      <p>Balance: {balance}</p>
     </div>
   );
 };
