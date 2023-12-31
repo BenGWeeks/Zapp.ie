4c68b506c38cb22ce1232953519db6a4e0d122e8
Ben Weeks <ben.weeks@outlook.com>
2023-12-31 20:35:26 +0000

    aider: Added links to Admin/Home, Bounties, Feed, Leaderboard, Rewards, ZapLog on the homepage as a menu bar.
    # Aider chat conversation:
    
    USER: Let's a links to Admin/Home, Bounties, Feed, Leaderboard, Rewards, ZapLog on the homepage of the app as a menu bar.
    
    ASSISTANT: To add links to Admin/Home, Bounties, Feed, Leaderboard, Rewards, ZapLog on the homepage of the app as a menu bar, we need to modify `src/App.js`.
    
    Here is the *SEARCH/REPLACE* block:
    
    src/App.js
    ```javascript
    <<<<<<< SEARCH
    import React from 'react';
    import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
    import Home from './pages/admin/Home';
    
    const App = () => {
      return (
        <Router>
          <div>
            <h1>Hello, world!</h1>
            <Link to="/admin">Admin</Link>
            <Route path="/admin" component={Home} />
          </div>
        </Router>
      );
    };
    
    export default App;
    =======
    import React from 'react';
    import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
    import Home from './pages/admin/Home';
    import Bounties from './pages/Bounties';
    import Feed from './pages/Feed';
    import Leaderboard from './pages/Leaderboard';
    import Rewards from './pages/Rewards';
    import ZapLog from './pages/ZapLog';
    
    const App = () => {
      return (
        <Router>
          <div>
            <h1>Hello, world!</h1>
            <nav>
              <Link to="/admin">Admin</Link>
              <Link to="/bounties">Bounties</Link>
              <Link to="/feed">Feed</Link>
              <Link to="/leaderboard">Leaderboard</Link>
              <Link to="/rewards">Rewards</Link>
              <Link to="/zaplog">ZapLog</Link>
            </nav>
            <Route path="/admin" component={Home} />
            <Route path="/bounties" component={Bounties} />
            <Route path="/feed" component={Feed} />
            <Route path="/leaderboard" component={Leaderboard} />
            <Route path="/rewards" component={Rewards} />
            <Route path="/zaplog" component={ZapLog} />
          </div>
        </Router>
      );
    };
    
    export default App;
    >>>>>>> REPLACE
    ```
    
    Please note that this change assumes that the components `Bounties`, `Feed`, `Leaderboard`, `Rewards`, `ZapLog` exist in the `src/pages` directory. If they do not exist, you will need to create them.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './pages/admin/Home';
+import Bounties from './pages/Bounties';
+import Feed from './pages/Feed';
+import Leaderboard from './pages/Leaderboard';
+import Rewards from './pages/Rewards';
+import ZapLog from './pages/ZapLog';
 
 const App = () => {
   return (
     <Router>
       <div>
         <h1>Hello, world!</h1>
-        <Link to="/admin">Admin</Link>
+        <nav>
+          <Link to="/admin">Admin</Link>
+          <Link to="/bounties">Bounties</Link>
+          <Link to="/feed">Feed</Link>
+          <Link to="/leaderboard">Leaderboard</Link>
+          <Link to="/rewards">Rewards</Link>
+          <Link to="/zaplog">ZapLog</Link>
+        </nav>
         <Route path="/admin" component={Home} />
+        <Route path="/bounties" component={Bounties} />
+        <Route path="/feed" component={Feed} />
+        <Route path="/leaderboard" component={Leaderboard} />
+        <Route path="/rewards" component={Rewards} />
+        <Route path="/zaplog" component={ZapLog} />
       </div>
     </Router>
   );
