3c225a327039940d74d351e36e8748b59deef4c3
Ben Weeks <ben.weeks@outlook.com>
2024-01-01 18:44:47 +0000

    Add carousel styling to Rewards page.

--- client/src/pages/Rewards.js
+++ client/src/pages/Rewards.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import { DocumentCard, DocumentCardImage, DocumentCardTitle, DocumentCardDetails, PrimaryButton } from '@fluentui/react';
 
