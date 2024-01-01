017f2b307d9ddd92bfd5903ebecd255351405576
Ben Weeks <ben.weeks@outlook.com>
2024-01-01 13:31:11 +0000

    Removed example table creation from create.sql.

--- database/create.sql
+++ database/create.sql
@@ -1,9 +1,4 @@
 -- Write your CREATE SQL statements here
-CREATE TABLE example (
-    id INTEGER PRIMARY KEY,
-    data TEXT
-);
-
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
