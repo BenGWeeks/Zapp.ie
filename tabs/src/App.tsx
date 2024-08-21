5003df4ece7c177e99af3431a040b4707ba227e7
Ben Weeks <ben.weeks@outlook.com>
2024-08-21 21:33:13 +0100

    Converted to TypeScript react web app from JavaScript

---
+++ tabs/src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.tsx
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello world!</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
