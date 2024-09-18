537251dd4360694d983e04c26e83439f0d00ff74
Ben Weeks <ben.weeks@outlook.com>
2024-09-18 02:16:14 +0100

    Resolving auth token issue
    Looks like repeated calls get blocked by the browser.

--- tabs/src/components/FeedComponent.tsx
+++ tabs/src/components/FeedComponent.tsx
@@ -4,30 +4,14 @@ import FeedList from './FeedList';
 import React from 'react';
 
 const FeedComponent: FunctionComponent = () => {
-  const [timestamp, setTimestamp] = useState<number>(0);
-  const [activePeriod, setActivePeriod] = useState<number | null>(7); // Set default to 7-days
-
-  const onTabContainerClick = useCallback(() => {
-    // Add your code here
-  }, []);
-
-  const today = new Date();
-  const formattedDate = today.toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  });
-  const formattedTime = today.toLocaleTimeString('en-US', {
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: true,
-  });
+  const [timestamp, setTimestamp] = useState(
+    Math.floor(Date.now() / 1000 - 7 * 24 * 60 * 60),
+  );
+  const [activePeriod, setActivePeriod] = useState(7); // Set default to 7-days
 
   const handlePeriodClick = (days: number) => {
-    const futureTimestamp = Date.now() / 1000 - days * 24 * 60 * 60;
-    setTimestamp(futureTimestamp);
+    setTimestamp(Math.floor(Date.now() / 1000 - days * 24 * 60 * 60));
     setActivePeriod(days);
-    console.log(`handlePeriodClick: futureTimestamp: ${futureTimestamp}`);
   };
 
   return (
@@ -43,29 +27,12 @@ const FeedComponent: FunctionComponent = () => {
             </div>
           </div>
         </div>
-        <div className={styles.tab1} onClick={onTabContainerClick}>
+        <div className={styles.tab1}>
           <div className={styles.base1}>
             <div className={styles.stringBadgeIconStack}>
               <div className={styles.stringTabTitle}>Leaderboard</div>
             </div>
           </div>
-        </div>
-      </div>
-      <div className={styles.infoStrip} style={{ display: 'none' }}>
-        <b className={styles.jan12020}>Jan 1, 2020 - Jan 30, 2020</b>
-        <div className={styles.dateRange}>Date range:</div>
-        <div className={styles.div}>|</div>
-        <b className={styles.all}>All</b>
-        <div className={styles.groups}>
-          <span>Groups:</span>
-        </div>
-        <div className={styles.div1}>|</div>
-        <b className={styles.all1}>All</b>
-        <div className={styles.location}>Location:</div>
-        <div className={styles.div2}>|</div>
-        <div className={styles.amPt}>{formattedTime} (Local Time)</div>
-        <div className={styles.dateContainer}>
-          <b>{formattedDate}</b>
         </div>
       </div>
       <div className={styles.pivotPointsdoubleFull60}>
