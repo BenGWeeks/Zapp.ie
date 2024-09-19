c6159af3389536f88c451ad9b921f9571291fcb8
Ben Weeks <ben.weeks@outlook.com>
2024-09-19 16:44:10 +0000

    Tweaked Activity chart period to fit

--- tabs/src/Home.tsx
+++ tabs/src/Home.tsx
@@ -5,7 +5,7 @@ import ZapActivityChartComponent from './components/ZapActivityChartComponent';
 const Home: React.FC = () => {
   const inKey = 'ca04dc4dbc114b298f6d121b1d4ffc8e'; // Hardcoded to my wallet for now.
   const [timestamp] = useState(() => {
-    return Math.floor(Date.now() / 1000) - 60 * 60 * 24 * 365 * (9 / 12); // Last 9 months
+    return Math.floor(Date.now() / 1000) - 60 * 60 * 24 * 365 * (8.5 / 12); // Last 8.5 months
   });
 
   return (
