fa9688da8f288e7c2c1f7ab6004e65ddd3f02901
Ben Weeks <ben.weeks@outlook.com>
2024-09-19 14:15:51 +0000

    Resolved failed to fetch issue

--- tabs/src/components/FooterComponent.tsx
+++ tabs/src/components/FooterComponent.tsx
@@ -5,7 +5,10 @@ import styles from './FooterComponent.module.css';
 const FooterComponent: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <Link to="/">Home</Link> | <Link to="/users">Users</Link>
+      <Link to="/">Home</Link>&nbsp;|&nbsp;
+      <Link to="/users">Users</Link>&nbsp;|&nbsp; 
+      <Link to="/wallet">Wallet</Link>&nbsp;|&nbsp;
+      <Link to="/yourwallet">Your Wallet</Link>
     </footer>
   );
 };
