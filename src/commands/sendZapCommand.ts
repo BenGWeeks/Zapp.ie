bb18437086ecfbc8f51673126374262b8ce328e5
Bohdan <bogdan.bratsiuk@gmail.com>
2024-12-31 14:28:51 +0100

    Bug 97121: Bot notification message is duplicating
    Co-Authored-By: EdiWeeks <121399761+EdiWeeks@users.noreply.github.com>

--- src/commands/sendZapCommand.ts
+++ src/commands/sendZapCommand.ts
@@ -340,11 +340,6 @@ async function messageRecipient(
       error.statusCode === 403 ||
       error.message.includes('Bot not in conversation roster')
     ) {
-      // Inform the sender that the recipient hasn't installed the bot
-      await context.sendActivity(
-        `FYI I wasn't able to message ${receiver.displayName} that they have a zap from you because they don't have me installed yet - maybe you could ping them, and let them know to install Zapp.ie!`,
-      );
-    } else {
       console.error('Error in messageRecipient:', error);
       throw error;
     }
