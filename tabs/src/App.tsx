ec0bcd97e57c9fd8dadd97aa1132bfbfa96c0328
desousamario85 <desousa.mario@yahoo.com>
2025-03-11 11:09:07 +0000

    fixing app.tsx with latest

--- tabs/src/App.tsx
+++ tabs/src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation  } from 'react-router-dom';
+import { useEffect } from 'react';
 // Fluent UI imports
 import { Stack } from '@fluentui/react';
 
@@ -18,85 +19,60 @@ import Users from './Users';
 import './App.css';
 import Rewards from './Rewards';
 import Wallet from './Wallet';
-import Settings from './Settings';
-
-//Importing settings
-import { RewardNameProvider } from './components/RewardNameContext';
-
 import AuthStart from './AuthStart';
 import AuthEnd from './AuthEnd';
-
+import Settings from './Settings';
 
 
 type AppProps = {
   pca: IPublicClientApplication;
 };
 
-function App({ pca }: AppProps) {
-  return (
+// Function to update the title based on the current route
+function TitleUpdater() {
+  const location = useLocation();
 
-    <MsalProvider instance={pca}>
-      <RewardNameProvider>
-      <PageLayout>
-        <Stack horizontalAlign="center">
-          <Pages />
-        </Stack>
-      </PageLayout>
-      </RewardNameProvider>
+  useEffect(() => {
+    const titles: { [key: string]: string } = {
+      "/feed": "Feed - Zapp.ie",
+      "/users": "Users - Zapp.ie",
+      "/rewards": "Rewards - Zapp.ie",
+      "/wallet": "Wallet - Zapp.ie",
+      "/login": "Login - Zapp.ie",
+      "/auth-start": "Authenticating...",
+      "/auth-end": "Authentication Complete",
+      "/settings": "Settings - Zapp.ie",
+    };
 
-    </MsalProvider>
-  );
+    document.title = titles[location.pathname] || "Zapp.ie"; 
+  }, [location]);
+
+  return null;
 }
 
 
-function Pages() {
+
+function App({ pca }: AppProps) {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route
-        path="/feed"
-        element={
-          <RequireAuth>
-            <Feed />
-          </RequireAuth>
-        }
-      />
-      <Route
-        path="/users"
-        element={
-          <RequireAuth>
-            <Users />
-          </RequireAuth>
-        }
-      />
-      <Route
-        path="/Rewards"
-        element={
-          <RequireAuth>
-            <Rewards />
-          </RequireAuth>
-        }
-      />
-      <Route
-        path="/Wallet"
-        element={
-          <RequireAuth>
-            <Wallet />
-          </RequireAuth>
-        }
-      />
-            <Route
-        path="/Settings"
-        element={
-          <RequireAuth>
-            <Settings />
-          </RequireAuth>
-        }
-      />
-      
-    </Routes>
+    <MsalProvider instance={pca}> 
+    <TitleUpdater />
+     <PageLayout> 
+     <Stack horizontalAlign="center">    
+        <Routes>
+          <Route path="/feed" element={<RequireAuth><Feed /></RequireAuth>} />
+          <Route path="/users" element={<RequireAuth><Users /></RequireAuth>} />
+          <Route path="/rewards" element={<RequireAuth><Rewards /></RequireAuth>} />
+          <Route path="/wallet" element={<RequireAuth><Wallet /></RequireAuth>} />
+          <Route path="/settings" element={<RequireAuth><Settings /></RequireAuth>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/auth-start" element={<AuthStart />} />
+          <Route path="/auth-end" element={<AuthEnd />} />
+          <Route path="*" element={<Navigate to="/feed" replace />} />         
+        </Routes>
+      </Stack>
+      </PageLayout>
+     </MsalProvider>
   );
 }
 
 export default App;
-
